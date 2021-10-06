3da7c115e6c6bda46697597cb8a3a263450408b2
SachinPitale <sachinpitale22@gmail.com>
2021-10-06 17:24:33 +0530

    add vnet

--- 01-Azure-Virtual-Network-4Tier/c1-versions.tf
+++ 01-Azure-Virtual-Network-4Tier/c1-versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
       azurerm = {
-          source = "hasicorp/azurerm"
+          source = "hashicorp/azurerm"
           version = ">= 2.0"
       }
       random = {
