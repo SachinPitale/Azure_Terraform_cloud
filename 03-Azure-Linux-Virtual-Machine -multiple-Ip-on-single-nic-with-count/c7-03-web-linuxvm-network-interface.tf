0de25e927ca297f5b0d3a53b359755c3e9ef3ed1
SachinPitale <sachinpitale22@gmail.com>
2021-10-07 18:56:08 +0530

    azure vm with two public ip

--- 03-Azure-Linux-Virtual-Machine -multiple-Ip-on-single-nic-with-count/c7-03-web-linuxvm-network-interface.tf
+++ 03-Azure-Linux-Virtual-Machine -multiple-Ip-on-single-nic-with-count/c7-03-web-linuxvm-network-interface.tf
@@ -1,7 +1,7 @@
 # Resource-2: Create Network Interface
 resource "azurerm_network_interface" "web_linuxvm_nic" {
   
-  name = "${local.resource_group_prefix}-web-linuxvm-nic-${count.index}"
+  name = "${local.resource_group_prefix}-web-linuxvm-nic"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
   ip_configuration {
