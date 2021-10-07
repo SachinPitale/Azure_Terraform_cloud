ceb6e9c07c7d7086299ba86897ce8ade6c7ca626
SachinPitale <sachinpitale22@gmail.com>
2021-10-07 18:22:12 +0530

    azure vm

---
+++ 02-Azure-Linux-Virtual-Machine/c7-02-web-linuxvm-publicip.tf
@@ -0,0 +1,10 @@
+# Resource-1: Create Public IP Address
+resource "azurerm_public_ip" "web_linuxvm_publicip" {
+  name = "${local.resource_group_prefix}-web-linuxvm-publicip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  allocation_method = "Static"
+  domain_name_label = "web1-vm-${random_string.myrandom}"
+  sku = "Standard"
+  
+}
