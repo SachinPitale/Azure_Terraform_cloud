9601831f95fa3ea0319f8ffe649b9ebaae997af6
SachinPitale <sachinpitale22@gmail.com>
2021-10-07 19:53:46 +0530

    Multiple Azure VM

---
+++ 05-Azure-Multiple-Linux-Virtual-Machine/c7-01-web-linuxvm-input-variables.tf
@@ -0,0 +1,8 @@
+# Linux VM Input Variables Placeholder file.
+
+variable "Virtual_machine_instance" {
+  description = "Specify the number of vm you want to provision"
+  type = number
+  default = 2
+  
+}
---
+++ 05-Azure-Multiple-Linux-Virtual-Machine/c7-02-web-linuxvm-publicip.tf
@@ -0,0 +1,12 @@
+# Resource-1: Create Public IP Address
+resource "azurerm_public_ip" "web_linuxvm_publicip" {
+  count = var.Virtual_machine_instance
+  name = "${local.resource_group_prefix}-web-linuxvm-publicip-${count.index}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  allocation_method = "Static"
+  domain_name_label = "web1-vm-${count.index}-${random_string.myrandom.id}"
+  sku = "Standard"
+  
+}
+
