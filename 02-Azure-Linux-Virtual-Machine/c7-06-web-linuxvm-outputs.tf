0c4b1d996fe407380daee3d106b3dab0c287113a
SachinPitale <sachinpitale22@gmail.com>
2021-10-07 18:49:16 +0530

    azure vm

--- 02-Azure-Linux-Virtual-Machine/c7-06-web-linuxvm-outputs.tf
+++ 02-Azure-Linux-Virtual-Machine/c7-06-web-linuxvm-outputs.tf
@@ -1,13 +1,13 @@
 ## Public IP Address
 output "Public_IP_Address" {
   description = "Web Linux VM Public Address"
-  value = azurerm_public_ip.web_linuxvm_publicip.ip_address
+  value = azurerm_public_ip.web_linuxvm_publicip[*].ip_address
 }
 
 # Network Interface Outputs
 output "Network_Interface_id" {
   description = "Web Linux VM Network Interface ID"
-  value = azurerm_network_interface.web_linuxvm_nic.id
+  value = azurerm_network_interface.web_linuxvm_nic[*].id
 }
 
 ## Network Interface Private IP Addresses
@@ -15,7 +15,7 @@ output "Network_Interface_id" {
 ## Network Interface Private IP Addresses
 output "web_linuxvm_network_interface_private_ip_addresses" {
   description = "Web Linux VM Private IP Addresses"
-  value = [azurerm_network_interface.web_linuxvm_nic.private_ip_addresses]
+  value = [azurerm_network_interface.web_linuxvm_nic[*].private_ip_addresses]
 }
 
 # Linux VM Outputs
@@ -23,14 +23,14 @@ output "web_linuxvm_network_interface_private_ip_addresses" {
 ## Virtual Machine Public IP
 output "web_linuxvm_public_ip_address" {
   description = "Web Linux Virtual Machine Public IP"
-  value = azurerm_linux_virtual_machine.web-linux-vm.public_ip_address
+  value = azurerm_linux_virtual_machine.web-linux-vm[*].public_ip_address
 }
 
 
 ## Virtual Machine Private IP
 output "web_linuxvm_private_ip_address" {
   description = "Web Linux Virtual Machine Private IP"
-  value = azurerm_linux_virtual_machine.web-linux-vm.private_ip_address
+  value = azurerm_linux_virtual_machine.web-linux-vm[*].private_ip_address
 }
 ## Virtual Machine 128-bit ID
 output "web_linuxvm_virtual_machine_id_128bit" {
