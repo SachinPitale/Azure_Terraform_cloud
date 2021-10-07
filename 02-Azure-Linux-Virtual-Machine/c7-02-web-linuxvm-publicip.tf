fdb18cc8bf73ecd0545ac9a3adbba62588768b3f
SachinPitale <sachinpitale22@gmail.com>
2021-10-07 18:27:57 +0530

    azure vm

--- 02-Azure-Linux-Virtual-Machine/c7-02-web-linuxvm-publicip.tf
+++ 02-Azure-Linux-Virtual-Machine/c7-02-web-linuxvm-publicip.tf
@@ -4,7 +4,7 @@ resource "azurerm_public_ip" "web_linuxvm_publicip" {
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
   allocation_method = "Static"
-  domain_name_label = "web1-vm-${random_string.myrandom}"
+  domain_name_label = "web1-vm-${random_string.myrandom.id}"
   sku = "Standard"
   
 }
