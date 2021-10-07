8c388c8f56fb798ca95ad06ad98993f219beca93
SachinPitale <sachinpitale22@gmail.com>
2021-10-07 18:46:10 +0530

    azure vm with two public ip

---
+++ 03-Azure-Linux-Virtual-Machine -multiple-Ip-on-single-nic-with-count/c7-03-web-linuxvm-network-interface.tf
@@ -0,0 +1,16 @@
+# Resource-2: Create Network Interface
+resource "azurerm_network_interface" "web_linuxvm_nic" {
+  count = 2
+  name = "${local.resource_group_prefix}-web-linuxvm-nic-${count.index}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  ip_configuration {
+    name = "web1-linuxvm-ip-${count.index}"
+    private_ip_address_allocation = "Dynamic"
+    subnet_id = azurerm_subnet.websubnet.id
+    public_ip_address_id = element(azurerm_public_ip.web_linuxvm_publicip[*].id, count.index)
+  }
+  
+
+  
+}
