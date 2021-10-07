5abf30254b471b928849f3431fb641d2dbdf5932
SachinPitale <sachinpitale22@gmail.com>
2021-10-07 20:06:35 +0530

    Multiple Azure VM

--- 05-Azure-Multiple-Linux-Virtual-Machine/c6-03-web-subnet-and-nsg.tf
+++ 05-Azure-Multiple-Linux-Virtual-Machine/c6-03-web-subnet-and-nsg.tf
@@ -38,7 +38,7 @@ resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
   for_each = local.web_inbound_ports_map
   name                        = "Rule-Port-${each.value}"
   priority                    = each.key
-  direction                   = "Outbound"
+  direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
