73873070893cfb7528d43f52cca67a5c28bf7291
SachinPitale <sachinpitale22@gmail.com>
2021-10-07 18:27:10 +0530

    azure vm

--- 02-Azure-Linux-Virtual-Machine/c7-06-web-linuxvm-outputs.tf
+++ 02-Azure-Linux-Virtual-Machine/c7-06-web-linuxvm-outputs.tf
@@ -23,22 +23,22 @@ output "web_linuxvm_network_interface_private_ip_addresses" {
 ## Virtual Machine Public IP
 output "web_linuxvm_public_ip_address" {
   description = "Web Linux Virtual Machine Public IP"
-  value = azurerm_linux_virtual_machine.web_linuxvm.public_ip_address
+  value = azurerm_linux_virtual_machine.web-linux-vm.public_ip_address
 }
 
 
 ## Virtual Machine Private IP
 output "web_linuxvm_private_ip_address" {
   description = "Web Linux Virtual Machine Private IP"
-  value = azurerm_linux_virtual_machine.web_linuxvm.private_ip_address
+  value = azurerm_linux_virtual_machine.web-linux-vm.private_ip_address
 }
 ## Virtual Machine 128-bit ID
 output "web_linuxvm_virtual_machine_id_128bit" {
   description = "Web Linux Virtual Machine ID - 128-bit identifier"
-  value = azurerm_linux_virtual_machine.web_linuxvm.virtual_machine_id
+  value = azurerm_linux_virtual_machine.web-linux-vm.virtual_machine_id
 }
 ## Virtual Machine ID
 output "web_linuxvm_virtual_machine_id" {
   description = "Web Linux Virtual Machine ID "
-  value = azurerm_linux_virtual_machine.web_linuxvm.id
+  value = azurerm_linux_virtual_machine.web-linux-vm.id
 }
