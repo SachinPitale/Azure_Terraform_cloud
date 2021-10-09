3d5b8665415be2f9ced20fabd0c7970636aece13
SachinPitale <sachinpitale22@gmail.com>
2021-10-09 13:29:48 +0530

    Azure-Bastion-Service-and-Host

--- 06-Azure-Bastion-Service-and-Host/c8-05-bastion-outputs.tf
+++ 06-Azure-Bastion-Service-and-Host/c8-05-bastion-outputs.tf
@@ -1,6 +1,6 @@
 
 ## Bastion Host Public IP Output
 output "bastion_host_linuxvm_public_ip" {
-  value = "bastion host linux vm public ip"
-  default = azurerm_linux_virtual_machine.Bastion_host_linuxvm-1.ip_address
+  description  = "bastion host linux vm public ip"
+  value  = azurerm_linux_virtual_machine.Bastion_host_linuxvm-1.ip_address
 }
