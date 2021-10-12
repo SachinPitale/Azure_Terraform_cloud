a20a557bdad134ac998ac65da6041b11e0d78e82
SachinPitale <sachinpitale22@gmail.com>
2021-10-12 12:38:32 +0530

    vmss manual scale

--- 11-Azure-VM-ScaleSets-Manual-scaling/c7-03-web-linux-vmss-resource.tf
+++ 11-Azure-VM-ScaleSets-Manual-scaling/c7-03-web-linux-vmss-resource.tf
@@ -24,7 +24,7 @@ CUSTOM_DATA
 
 # Resource: Azure Linux Virtual Machine Scale Set - web1
 resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
-  name = "${local.resource_group_prefix}-web_vmss"
+  name = "${local.resource_group_prefix}-web-vmss"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
   sku = "Standard_DS1_v2"
