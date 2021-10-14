99dadadc466ca96790cd8812fed8dd79719101e9
SachinPitale <sachinpitale22@gmail.com>
2021-10-14 17:16:05 +0530

    traffic manager

--- 16-Azure-Traffic-Manager/project-3-azure-traffic-manager/c6-traffic-manager.tf
+++ 16-Azure-Traffic-Manager/project-3-azure-traffic-manager/c6-traffic-manager.tf
@@ -28,7 +28,7 @@ resource "azurerm_traffic_manager_endpoint" "tm_endpoint_project1_eastus2" {
   profile_name = azurerm_traffic_manager_profile.web_app_profile.name
   type = "azureEndpoints"
   weight = 50
-  target_resource_id = data.erraform_remote_state.project1_eastus2.outputs.web_lb_public_ip_address_id
+  target_resource_id = data.terraform_remote_state.project1_eastus2.outputs.web_lb_public_ip_address_id
   
 }
 
