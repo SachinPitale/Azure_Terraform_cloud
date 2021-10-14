d8a877e5ad53154235de23f213ed61fc55456b2a
SachinPitale <sachinpitale22@gmail.com>
2021-10-14 20:32:32 +0530

    azure AG

---
+++ 17-Azure-Application-Gateway-Basics/c8-05-bastion-outputs.tf
@@ -0,0 +1,6 @@
+
+## Bastion Host Public IP Output
+output "bastion_host_linuxvm_public_ip" {
+  description  = "bastion host linux vm public ip"
+  value  = azurerm_public_ip.Bastion_host_Public_IP.ip_address
+}
