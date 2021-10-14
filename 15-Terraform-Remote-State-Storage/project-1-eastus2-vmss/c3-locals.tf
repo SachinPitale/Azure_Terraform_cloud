8c00a8a3cf753cf05f9cbecb06c844852746e0d0
SachinPitale <sachinpitale22@gmail.com>
2021-10-14 14:50:21 +0530

    backed storage account

--- 15-Terraform-Remote-State-Storage/project-1-eastus2-vmss/c3-locals.tf
+++ 15-Terraform-Remote-State-Storage/project-1-eastus2-vmss/c3-locals.tf
@@ -4,7 +4,7 @@ locals {
   owners = var.business_divsion
   environment = var.environment
   #resource_group_prefix = "${var.business_divsion}-${var.environment}"
-  resource_group_prefix = "${var.business_divsion}-${var.business_divsion}-${var.environment}"
+  resource_group_prefix = "${var.resource_group_location}-${var.business_divsion}-${var.environment}"
   common_tags = {
       owners = local.owners
       environment = local.environment
