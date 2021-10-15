6848126333e889d6dc78c01332550cc83bfd1f77
SachinPitale <sachinpitale22@gmail.com>
2021-10-15 13:00:41 +0530

    multi side webhosting

---
+++ 19-Azure-Application-Gateway-Multisite-Hosting/c7-05-web-linux-vmss-app2-nsg-inline-basic.tf
@@ -0,0 +1,26 @@
+
+# Create Network Security Group using Terraform Dynamic Blocks
+resource "azurerm_network_security_group" "app2_web_vmss_nsg" {
+  name = "${local.resource_group_prefix}-app2_web_vmss_nsg"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+   
+  dynamic "security_rule" {
+    for_each = var.app2_web_vmss_nsg_inbound_ports
+    content {
+      name                       = "Allow_port_for-${security_rule.value}"
+      description                = "Inbound Rule ${security_rule.key}" 
+      priority                   = sum([100, security_rule.key])
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = security_rule.value
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+
+  }
+
+  
+}
