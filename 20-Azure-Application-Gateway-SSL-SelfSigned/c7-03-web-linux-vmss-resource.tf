c03c0f75f8ffbec3840490bd77e2852f626418f4
SachinPitale <sachinpitale22@gmail.com>
2021-10-15 16:55:03 +0530

    AG ssl

--- 20-Azure-Application-Gateway-SSL-SelfSigned/c7-03-web-linux-vmss-resource.tf
+++ 20-Azure-Application-Gateway-SSL-SelfSigned/c7-03-web-linux-vmss-resource.tf
@@ -55,7 +55,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
       name = "internal"
       primary = true
       subnet_id = azurerm_subnet.websubnet.id
-      load_balancer_backend_address_pool_ids = [ azurerm_lb_backend_address_pool.web1_backend_pool.id ]
+       application_gateway_backend_address_pool_ids = [ azurerm_lb_backend_address_pool.web1_backend_pool.id ]
     }
   }
 
